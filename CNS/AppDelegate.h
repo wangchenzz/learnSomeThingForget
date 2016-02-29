89ea3d1e51566ef47e40e1186965d87dc3f7610c
wangchenzz <Mac@macdeimac.local>
2016-02-29 14:14:45 +0800

    Initial Commit

---
+++ CNS/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CNS
+//
+//  Created by Mac on 16/2/29.
+//  Copyright © 2016年 竞思教育. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
